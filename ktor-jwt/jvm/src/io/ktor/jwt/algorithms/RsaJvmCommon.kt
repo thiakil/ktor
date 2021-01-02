0df89caf65e1840667018a85bb916e9eb950cd0c
Thiakil <xander@thiakil.com>
2021-01-02 18:58:23 +0800

    add JVM->JWK conversion for RSA, use more RSA Private Key fields when they're present

--- ktor-jwt/jvm/src/io/ktor/jwt/algorithms/RsaJvmCommon.kt
+++ ktor-jwt/jvm/src/io/ktor/jwt/algorithms/RsaJvmCommon.kt
@@ -5,7 +5,7 @@
 package io.ktor.jwt.algorithms
 
 import io.ktor.jwt.*
-import io.ktor.util.*
+import org.jetbrains.annotations.*
 import java.math.*
 import java.security.*
 import java.security.interfaces.*
@@ -15,12 +15,14 @@ public data class JavaRSAKey(val publicKey: RSAPublicKey? = null, val privateKey
     public constructor(keyPair: KeyPair): this(keyPair.public as RSAPublicKey?, keyPair.private as RSAPrivateKey?)
 }
 
+private fun ByteArray.toUintBigInteger(): BigInteger = BigInteger(1, this)
+
 public fun JsonWebKey.RSA.toJavaRSAPublicKey(): RSAPublicKey {
     if (!this.isValidPublicKey) {
         throw UnsupportedKeyException("Not a valid RSA public key")
     }
-    val modulus = BigInteger(1, this.modulus!!.decodeBase64UrlBytes())
-    val exponent = BigInteger(1, this.exponent!!.decodeBase64UrlBytes())
+    val modulus = this.modulus!!.toUintBigInteger()
+    val exponent = this.exponent!!.toUintBigInteger()
     try {
         return KeyFactory.getInstance("RSA").generatePublic(RSAPublicKeySpec(modulus, exponent)) as RSAPublicKey
     } catch (e: InvalidKeySpecException) {
@@ -34,13 +36,124 @@ public fun JsonWebKey.RSA.toJavaRSAPrivateKey(): RSAPrivateKey {
     if (!this.isValidPrivateKey) {
         throw UnsupportedKeyException("Not a valid RSA private key")
     }
-    val modulus = BigInteger(1, this.modulus!!.decodeBase64UrlBytes())
-    val exponent = BigInteger(1, this.privateExponent!!.decodeBase64UrlBytes())
+    val modulus = this.modulus!!.toUintBigInteger()
+    val publicExponent = this.exponent!!.toUintBigInteger()
+    val privateExponent = this.privateExponent!!.toUintBigInteger()
+    val firstPrimeFactor = this.firstPrimeFactor?.toUintBigInteger()
+    val secondPrimeFactor = this.secondPrimeFactor?.toUintBigInteger()
+    val firstFactorCRTExponent = this.firstFactorCRTExponent?.toUintBigInteger()
+    val secondFactorCRTExponent = this.secondFactorCRTExponent?.toUintBigInteger()
+    val firstCRTCoefficient = this.firstCRTCoefficient?.toUintBigInteger()
+    val otherPrimesInfo = this.otherPrimesInfo?.map {
+        RSAOtherPrimeInfo(it.primeFactor.toUintBigInteger(), it.factorCRTExponent.toUintBigInteger(), it.factorCRTCoefficient.toUintBigInteger())
+    }?.toTypedArray()
     try {
-        return KeyFactory.getInstance("RSA").generatePrivate(RSAPrivateKeySpec(modulus, exponent)) as RSAPrivateKey
+        val keySpec = if (firstPrimeFactor != null && secondPrimeFactor != null && firstFactorCRTExponent != null && secondFactorCRTExponent != null && firstCRTCoefficient != null) {
+            if (otherPrimesInfo.isNullOrEmpty()){
+                RSAPrivateCrtKeySpec(modulus, publicExponent, privateExponent, firstPrimeFactor, secondPrimeFactor, firstFactorCRTExponent, secondFactorCRTExponent, firstCRTCoefficient)
+            } else {
+                RSAMultiPrimePrivateCrtKeySpec(modulus, publicExponent, privateExponent, firstPrimeFactor, secondPrimeFactor, firstFactorCRTExponent, secondFactorCRTExponent, firstCRTCoefficient, otherPrimesInfo)
+            }
+        } else {
+            RSAPrivateKeySpec(modulus, privateExponent)
+        }
+        return KeyFactory.getInstance("RSA").generatePrivate(keySpec) as RSAPrivateKey
     } catch (e: InvalidKeySpecException) {
         throw UnsupportedKeyException(e.message ?: "InvalidKeySpecException", e)
     } catch (e: NoSuchAlgorithmException) {
         throw UnsupportedKeyException(e.message ?: "NoSuchAlgorithmException", e)
     }
+}
+
+private fun ByteArray.toUnsigned(): ByteArray {
+    return if (this[0].toInt() == 0) {
+        // Strip the sign digit
+        val newArray = ByteArray(this.size - 1)
+        System.arraycopy(this, 1, newArray, 0, this.size - 1)
+        newArray
+    } else {
+        this
+    }
+}
+
+private fun BigInteger.toBytesUnsigned(): ByteArray = this.toByteArray().toUnsigned()
+
+/** Wrapper for the Crt key interfaces as multi-prime doesn't extend from RSAPrivateCrtKey */
+private interface CrtAdapter {
+    val primeP: BigInteger
+    val primeExponentP: BigInteger
+    val primeQ: BigInteger
+    val primeExponentQ: BigInteger
+    val crtCoefficient: BigInteger
+    val publicExponent: BigInteger
+}
+
+private class RSAPrivateCrtKeyAdapter(private val key: RSAPrivateCrtKey): CrtAdapter {
+    override val primeP: BigInteger
+        get() = key.primeP
+    override val primeExponentP: BigInteger
+        get() = key.primeExponentP
+    override val primeQ: BigInteger
+        get() = key.primeQ
+    override val primeExponentQ: BigInteger
+        get() = key.primeExponentQ
+    override val crtCoefficient: BigInteger
+        get() = key.crtCoefficient
+    override val publicExponent: BigInteger
+        get() = key.publicExponent
+}
+
+private class RSAMultiPrimePrivateCrtKeyAdapter(private val key: RSAMultiPrimePrivateCrtKey): CrtAdapter {
+    override val primeP: BigInteger
+        get() = key.primeP
+    override val primeExponentP: BigInteger
+        get() = key.primeExponentP
+    override val primeQ: BigInteger
+        get() = key.primeQ
+    override val primeExponentQ: BigInteger
+        get() = key.primeExponentQ
+    override val crtCoefficient: BigInteger
+        get() = key.crtCoefficient
+    override val publicExponent: BigInteger
+        get() = key.publicExponent
+}
+
+@Suppress("FunctionName")
+@Contract("null, null -> fail")
+public fun JsonWebKey.Companion.RSA(publicKey: RSAPublicKey? = null, privateKey: RSAPrivateKey? = null, keyId: String? = null): JsonWebKey.RSA {
+    if (publicKey == null && privateKey == null){
+        throw IllegalArgumentException("One of private or public keys must be supplied")
+    }
+    val modulus = when (publicKey) {
+        null -> privateKey!!.modulus!!
+        else -> publicKey.modulus!!
+    }
+    val privateCrt: CrtAdapter? = when (privateKey) {
+        is RSAPrivateCrtKey -> RSAPrivateCrtKeyAdapter(privateKey)
+        is RSAMultiPrimePrivateCrtKey -> RSAMultiPrimePrivateCrtKeyAdapter(privateKey)
+        else ->  null
+    }
+    val publicExponent = when (publicKey) {
+        null -> privateCrt?.publicExponent ?: error("Can't get public exponent from private key")
+        else -> publicKey.publicExponent
+    }
+    return JsonWebKey.RSA(
+        keyId = keyId,
+        modulus = modulus.toBytesUnsigned(),
+        exponent = publicExponent?.toBytesUnsigned(),
+        privateExponent = privateKey?.privateExponent?.toBytesUnsigned(),
+        firstPrimeFactor = privateCrt?.primeP?.toBytesUnsigned(),
+        firstFactorCRTExponent = privateCrt?.primeExponentP?.toBytesUnsigned(),
+        secondPrimeFactor = privateCrt?.primeQ?.toBytesUnsigned(),
+        secondFactorCRTExponent = privateCrt?.primeExponentQ?.toBytesUnsigned(),
+        firstCRTCoefficient = privateCrt?.crtCoefficient?.toBytesUnsigned(),
+        otherPrimesInfo = when (privateKey) {
+            is RSAMultiPrimePrivateCrtKey -> {
+                privateKey.otherPrimeInfo.map {
+                    JsonWebKey.RSA.OtherPrimeInfo(it.prime.toBytesUnsigned(), it.exponent.toBytesUnsigned(), it.crtCoefficient.toBytesUnsigned())
+                }
+            }
+            else -> null
+        }
+    )
 }
---
+++ ktor-jwt/jvm/test/io/ktor/jwt/algorithms/TestRsaJvmCommon.kt
@@ -0,0 +1,79 @@
+/*
+ * Copyright 2014-2021 JetBrains s.r.o and contributors. Use of this source code is governed by the Apache 2.0 license.
+ */
+
+package io.ktor.jwt.algorithms
+
+import io.ktor.jwt.*
+import io.ktor.jwt.algorithms.*
+import java.security.*
+import java.security.interfaces.*
+import kotlin.test.*
+
+class TestRsaJvmCommon {
+    @Test
+    fun testJvmToJwk(){
+        val keyPair = KeyPairGenerator.getInstance("RSA").apply {
+            initialize(2048, SecureRandom())
+        }.generateKeyPair()
+
+        //test public and private works
+        var newJwk = JsonWebKey.RSA(
+            privateKey = keyPair.private as RSAPrivateKey,
+            publicKey = keyPair.public as RSAPublicKey
+        )
+        assert(newJwk.isValidPrivateKey)
+        assert(newJwk.isValidPublicKey)
+
+        //test just public (produces only a valid public key)
+        newJwk = JsonWebKey.RSA(
+            publicKey = keyPair.public as RSAPublicKey
+        )
+
+        assert(newJwk.isValidPublicKey)
+        assertFalse(newJwk.isValidPrivateKey)
+
+        //test just private, which should have enough info for the public
+        newJwk = JsonWebKey.RSA(
+            privateKey = keyPair.private as RSAPrivateKey,
+        )
+        assert(newJwk.isValidPrivateKey)
+        assert(newJwk.isValidPublicKey)
+    }
+
+    @Test
+    fun testJvmJwkCompatibility() {
+        val keyPair = KeyPairGenerator.getInstance("RSA").apply {
+            initialize(2048, SecureRandom())
+        }.generateKeyPair()
+
+        val newJwk = JsonWebKey.RSA(
+            privateKey = keyPair.private as RSAPrivateKey,
+            publicKey = keyPair.public as RSAPublicKey
+        )
+        assert(newJwk.isValidPrivateKey)
+        assert(newJwk.isValidPublicKey)
+
+        val baseToken = makeJWT {
+            issuer = "test-issuer"
+            singleAudience = "test"
+            subject = "test testerton"
+        }
+
+        val nativeKey = JavaRSAKey(keyPair)
+
+        //test signing via native and verifying by jwk
+        val jvmSigned = baseToken.signSync {
+            key = nativeKey
+            alg = RS256
+        }
+        assert(RS256.verify(JWT.decode(jvmSigned).signature!!, newJwk))
+
+        //test signing via JWK and verifying via native
+        val jwkSigned = baseToken.signSync {
+            key = newJwk
+            alg = RS256
+        }
+        assert(RS256.verify(JWT.decode(jwkSigned).signature!!, nativeKey))
+    }
+}
